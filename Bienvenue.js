d0ae2b7454a4dfa3c71b1947fc69723ad00ee4cd
matteo-dab <153624968+matteo-dab@users.noreply.github.com>
2024-05-16 14:35:17 +0200

    Update and rename Bienvenu.js to Bienvenue.js

--- Bienvenue.js
+++ Bienvenue.js
@@ -1,18 +1,23 @@
+// Définition de la fonction getNom
 function getNom() {
+    // Effectuer une requête fetch vers le fichier nom.php
     fetch('nom.php')
-    .then(response => response.json()) // Convertir la réponse en JSON
+    // Traiter la réponse en tant que JSON
+    .then(response => response.json())
+    // Traiter les données JSON
     .then(Data => {
-        
+        // Vérifier si le champ nom est présent dans les données
         if (Data.nom) {
             // Affichage du message de bienvenue dans la balise <div> avec la classe "Bienvenu"
             document.querySelector('.Bienvenu').innerHTML = "Bienvenu " + Data.nom;
         }
-       
-        
     })
+    // Gérer les erreurs de la requête
     .catch(error => console.error('Erreur lors de la récupération du nom :', error));
 }
 
+// Exécuter la fonction getNom lorsque la fenêtre est entièrement chargée
 window.onload = function() {
     getNom();
 };
+
